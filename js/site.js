20deda556f54fc49663a83fe7489b5ddd71f434a
Michael Ebens <michael@nova-fusion.com>
2011-06-07 16:13:05 +1000

    Hopefully this will fix the delay in the download button.

--- js/site.js
+++ js/site.js
@@ -7,3 +7,9 @@ function getEmail()
   var a343t = "@nova";
   return px13452 + py23432 + a343t + nf323 + pw3;
 }
+
+function removeLinkListeners()
+{
+  var links = document.getElementsByTagName('a');
+  for (var i = 0; i < links.length; links[i++].removeEventListener('click', clicky.outbound));
+}
